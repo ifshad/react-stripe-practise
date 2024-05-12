2a5a1e258a1d391e4d9165e244a860af3d7ca5e7
ifshad <shoronshark@gmail.com>
2024-05-12 16:49:46 +0600

    react stripe added with client secret issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { loadStripe } from '@stripe/stripe-js';
 import './App.css';
+import { Elements } from '@stripe/react-stripe-js';
+import MyCheckoutForm from './components/MyCheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51PFX562LLzwpxMjdbnSupxnMz00GtXwrl3ChQ6dq7xWbi57fs66IJkzh7blLhwx3LQuva0oNoOecWx7yGAoyLyVf00OX8wn0Ry')
 
 function App() {
+  const options = {
+    clientSecret: process.env.CLIENT_SECRET,
+  }
   return (
     <div>
-      <h1>Welcome to stripe</h1>
+      <Elements stripe={stripePromise} options={options}>
+        <MyCheckoutForm />
+      </Elements>
     </div>
   );
 }
